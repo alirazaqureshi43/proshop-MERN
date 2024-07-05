d1678625f79b8c760e0711f8f8f8d22494cd3ada
Ali Raza <alirazaqureshi43@gmail.com>
2024-07-05 17:37:46 +0500

    adding-vercel.json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import connectDB from "./config/db.js";
 connectDB()
 const port = process.env.PORT || 5000;
 const app = express();
-
+ 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
@@ -24,8 +24,8 @@ app.get('/api/config/paypal', (req, res)=> res.send({
 }))
 app.use('/api/upload', uploadRoutes)
 
-// const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // if(process.env.NODE_ENV === 'production'){
 //     app.use(express.static(path.join(__dirname, '/frontend/build')))
