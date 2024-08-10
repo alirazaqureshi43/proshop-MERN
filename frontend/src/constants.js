993ff02a5a40a3a69c184f39f061712bcfe0b4bb
Ali Raza <alirazaqureshi43@gmail.com>
2024-08-10 16:17:21 +0500

    api-change

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
-export const BASE_URL = '';
-export const PRODUCTS_URL = '/api/products';
-export const UPLOAD_URL = '/api/upload';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const BASE_URL = 'https://proshop-backend-three.vercel.app';
+export const PRODUCTS_URL = 'https://proshop-backend-three.vercel.app/api/products';
+export const UPLOAD_URL = 'https://proshop-backend-three.vercel.app/api/upload';
+export const USERS_URL = 'https://proshop-backend-three.vercel.app/api/users';
+export const ORDERS_URL = 'https://proshop-backend-three.vercel.app/api/orders';
+export const PAYPAL_URL = 'https://proshop-backend-three.vercel.app/api/config/paypal';
